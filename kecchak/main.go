afea827b9c5fdb0d9dda8ccfc14a5a3ea5e485f7
zaindeveloper2024 <zain.developer2024@gmail.com>
2024-10-28 06:40:48 +0700

    kecchak with comment

--- kecchak/main.go
+++ kecchak/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"golang.org/x/crypto/sha3"
 )
 
 func main() {
 	hash := sha3.New256()
-	_, _ = hash.Write([]byte("Hello, World!"))
-
+	_, err := hash.Write([]byte("Hello, World!"))
+	if err != nil {
+		log.Fatalf("failed to write to hash: %v", err)
+	}
 	sha3 := hash.Sum(nil)
-
 	fmt.Printf("SHA3-256: %x\n", sha3)
 }
+
+// fileHash calculates the SHA3-256 hash of a file.
