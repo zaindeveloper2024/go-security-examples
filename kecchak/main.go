7ff7003f93828947f8792546b952916d49111c52
zaindeveloper2024 <zain.developer2024@gmail.com>
2024-10-28 21:27:50 +0700

    kecchak file hash

--- kecchak/main.go
+++ kecchak/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
+	"io"
 	"log"
+	"os"
 
 	"golang.org/x/crypto/sha3"
 )
+
+func generateFileSHA256(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", fmt.Errorf("error opening file: %s", err)
+	}
+	defer file.Close()
+
+	hash := sha3.New256()
+	if _, err := io.Copy(hash, file); err != nil {
+		return "", fmt.Errorf("error copying file: %s", err)
+	}
+
+	return hex.EncodeToString(hash.Sum(nil)), nil
+}
 
 func main() {
 	hash := sha3.New256()
@@ -15,6 +33,12 @@ func main() {
 	}
 	sha3 := hash.Sum(nil)
 	fmt.Printf("SHA3-256: %x\n", sha3)
-}
 
-// fileHash calculates the SHA3-256 hash of a file.
+	filepath := "go.mod"
+	hashedFile, err := generateFileSHA256(filepath)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		return
+	}
+	fmt.Printf("File: %s\nHashedFile: %s\n", filepath, hashedFile)
+}
